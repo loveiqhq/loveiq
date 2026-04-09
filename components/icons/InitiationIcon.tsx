91172f3a94a7a5137b10bbd9e2c8f38a47c4b6c8
FerhadJukicc <prokrom2008@gmail.com>
2026-04-09 23:40:23 +0200

    fixed several svgs

---
+++ components/icons/InitiationIcon.tsx
@@ -0,0 +1,12 @@
+import type { SVGProps } from "react";
+
+export default function InitiationIcon(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg viewBox="0 0 9 14" fill="none" aria-hidden="true" {...props}>
+      <path
+        d="M8.325 4.7H5.07937L6.2775 1.04938C6.39 0.621875 6.06656 0.2 5.625 0.2H1.575C1.2375 0.2 0.950625 0.450312 0.905625 0.785L0.005625 7.535C-0.0478125 7.94 0.267188 8.3 0.675 8.3H4.01344L2.71687 13.7703C2.61562 14.1978 2.94187 14.6 3.37219 14.6C3.60844 14.6 3.83344 14.4763 3.95719 14.2625L8.90719 5.7125C9.16875 5.26531 8.84531 4.7 8.325 4.7Z"
+        fill="currentColor"
+      />
+    </svg>
+  );
+}
